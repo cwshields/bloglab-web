0e9c034f224aef30f35d53a2bfd3430515049073
cwshields <cwshields2@gmail.com>
2023-05-26 02:02:28 -0500

    Changed file names

---
+++ src/pages/BlogCard/BlogCard.tsx
@@ -0,0 +1,46 @@
+import BlogProps from "../../types/BlogProps";
+import avatar from "../../assets/PngItem_4042710.png";
+import "../../sass/BlogCard.scss";
+import { useState } from "react";
+import { Button } from "react-bootstrap";
+
+export default function Blog(props: BlogProps) {
+  
+  const { title, description, user, date, readTime } = props;
+  const [bookmark, setBookmark] = useState(false);
+
+  const handleToggle = () => {
+    setBookmark((current) => !current);
+  };
+
+  return (
+    <div className="blog-card">
+      <h2 className="title">{title}</h2>
+      <div className="description">{description}</div>
+      <div className="tags-wrap">
+        <div className="tag"></div>
+        <div className="tag"></div>
+        <div className="tag"></div>
+      </div>
+      <div className="footer">
+        <div className="user-wrap">
+          <img className="avatar" src={avatar} alt="avatar" />
+          <div className="text-wrap">
+            <div className="user">{user}</div>
+            <div className="date">{date}</div>
+          </div>
+        </div>
+        <div className="right-wrap">
+          <div className="readtime">{readTime} minute read</div>
+          <button onClick={handleToggle} className="btn bookmark">
+            {bookmark ? (
+              <i className="fa-solid fa-bookmark"></i>
+              ) : (
+              <i className="fa-regular fa-bookmark"></i>
+            )}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
