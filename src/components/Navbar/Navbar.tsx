381745237b9d421bd4093c9f8f00601e85121e7c
cwshields <cwshields2@gmail.com>
2023-05-26 01:58:34 -0500

    Add searchbar  - Add signup button

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,19 +1,38 @@
-import '../../sass/Navbar.scss'
-import {ReactComponent as BlogLabLogo} from '../../assets/BlogLab-logo-light.svg'
+import "../../sass/Navbar.scss";
+import { ReactComponent as BlogLabLogo } from "../../assets/BlogLab-green-logo-dark.svg";
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <div className='navbar-container'>
-      <div className='navbar-left-wrap'>
-        <div className='logo-wrap'>
-          <BlogLabLogo width={150} />
+    <div className="navbar-container">
+      <div className="navbar-left-wrap">
+        <div className="logo-wrap">
+          <Link to="/">
+            <BlogLabLogo width={150} />
+          </Link>
         </div>
+        <form className="d-flex search-bar" role="search">
+          <input
+            className="form-control"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <button className="btn btn-outline-primary search-btn" type="submit">
+            <i className="fa-solid fa-magnifying-glass"></i>
+          </button>
+        </form>
       </div>
-      <div className='navbar-right-wrap'>
-        <Link to='/login' className='link'>Login</Link>
-        <button type="button" className="btn btn-outline-primary">Signup</button>
+      <div className="navbar-right-wrap">
+        <Link to="/login" className="link">
+          Login
+        </Link>
+        <Link to="/signup">
+          <button type="button" className="btn btn-outline-primary">
+            Signup
+          </button>
+        </Link>
       </div>
     </div>
-  )
+  );
 }
