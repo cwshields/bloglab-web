989659a8a54602123799e7565f1ac02db8185f8a
cwshields <cwshields2@gmail.com>
2023-06-02 22:30:00 -0500

    Add site title

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import './sass/App.scss';
-import Navbar from './components/Navbar/Navbar';
-import { BrowserRouter } from 'react-router-dom';
+import "./sass/App.scss";
+import Navbar from "./components/Navbar/Navbar";
+import { BrowserRouter } from "react-router-dom";
 import Router from "./pages/router";
+import React, { useEffect } from "react";
 
 export default function App() {
+  useEffect(() => {
+    document.title = "BlogLab";
+  }, []);
+
   return (
     <>
       <BrowserRouter>
