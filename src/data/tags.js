489f1309b1098f7bbf92bd45d15b575f2dbcf918
cwshields <cwshields2@gmail.com>
2023-06-11 14:13:47 -0500

    Add tag data

---
+++ src/data/tags.js
@@ -0,0 +1,76 @@
+const tags = [
+  {
+    name: "philosophy",
+    color: "orange",
+    description:
+      "Cupiditate architecto voluptatum esse laudantium, temporibus excepturi tempore iure voluptatem reprehenderit quod officiis delectus vero quaerat ab inventore aut consequuntur accusantium fugit alias autem quasi reiciendis aliquid nobis.",
+    guidelines:
+      "Necessitatibus ipsum deleniti adcum delectus quisquam officiis corrupti vero quod architecto fuga consequatur saepe quo explicabo cupiditate.",
+  },
+  {
+    name: "advice",
+    color: "green",
+    description:
+      "Quisquam, odit rem doloribus accusantium similique itaque aspernatur sunt! Pariatur cupiditate eveniet et? Perferendis, consequatur vitae corrupti mollitia aspernatur ducimus ut dolorem.",
+    guidelines:
+      "Eaque illum labore tempora atque quod quas assumenda maiores, sint voluptatem soluta, corrupti, distinctio dolores porro molestiae nam.",
+  },
+  {
+    name: "life",
+    color: "red",
+    description:
+      "Eaque illum labore tempora atque quod quas assumenda maiores, sint voluptatem soluta, corrupti, distinctio dolores porro molestiae nam. Ea minus maiores inventore, placeat assumenda voluptas qui amet iure vero officiis dignissimos culpa vitae similique vel sequi.",
+    guidelines:
+      "Temporibus excepturi tempore iure voluptatem reprehenderit quod officiis delectus vero quaerat ab inventore aut consequuntur accusantium fugit alias autem quasi reiciendis aliquid nobis.",
+  },
+  {
+    name: "storytime",
+    color: "violet",
+    description:
+      "Explicabo beatae iure obcaecati, suscipit molestiae cum quod earum quo necessitatibus est, ipsum eaque dolorum ducimus ex, facere aspernatur! Quisquam enim possimus quas earum in molestias eius nobis alias.",
+    guidelines:
+      "Ea minus maiores inventore, placeat assumenda voluptas qui amet iure vero officiis dignissimos culpa vitae similique vel sequi.",
+  },
+  {
+    name: "inspiration",
+    color: "black",
+    description:
+      "Necessitatibus ipsum deleniti adcum delectus quisquam officiis corrupti vero quod architecto fuga consequatur saepe quo explicabo cupiditate, porro odit aliquam quis vel unde? Ipsa, laborum? Deleniti, id. Amet, saepe inventore expedita solut explicabo tenetur.",
+    guidelines:
+      "Quam in fuga rem ad rerum nulla repellendus minima. Autem repudiandae dignissimos impedit debitis expedita consequatur illum commodi officia laborum illo.",
+  },
+  {
+    name: "selfhelp",
+    color: "red",
+    description:
+      "Reprehenderit quam in fuga rem ad rerum nulla repellendus minima. Autem repudiandae dignissimos impedit debitis expedita consequatur illum commodi officia laborum illo.",
+    guidelines:
+      "Beatae iure obcaecati, suscipit molestiae cum quod earum quo necessitatibus est, ipsum eaque dolorum ducimus ex, facere aspernatur! Quisquam enim possimus quas earum in molestias eius nobis alias.",
+  },
+  {
+    name: "jobs",
+    color: "blue",
+    description: "Ipsum eaque dolorum ducimus assumenda voluptas qui amet iure vero officiis dignissimos culpa vitae similique vel sequi",
+    guidelines:
+      "Aperiam quod voluptas ipsum cupiditate unde amet excepturi labore repudiandae ipsa voluptate quisquam porro quasi optio.",
+    about: "Temporibus excepturi tempore iure voluptatem reprehenderit quod officiis delectus vero quaerat ab inventore aut consequuntur accusantium fugit alias autem quasi reiciendis aliquid nobis.",
+  },
+  {
+    name: "society",
+    color: "teal",
+    description:
+      "Exercitationem quisquam aperiam in sint itaque sed tenetur dolor officia neque nemo, aspernatur debitis sequi asperiores qui aliquam soluta ex harum libero molestiae inventore officiis ut.",
+    guidelines:
+      "Magni autem nesciunt earum culpa repellat harum aperiam quod voluptas ipsum cupiditate unde amet excepturi labore repudiandae ipsa voluptate quisquam porro quasi optio, iste neque beatae.",
+  },
+  {
+    name: "technology",
+    color: "gray",
+    description:
+      "Magni autem nesciunt earum culpa repellat harum aperiam quod voluptas ipsum cupiditate unde amet excepturi labore repudiandae ipsa voluptate quisquam porro quasi optio, iste neque beatae. Atque minus rerum error fugiat nisi officiis, unde fuga adipisci.",
+    guidelines:
+      "Quisquam aperiam in sint itaque sed tenetur dolor officia neque nemo, aspernatur debitis sequi asperiores qui aliquam soluta ex harum libero molestiae inventore officiis ut.",
+  },
+];
+
+export default tags;
