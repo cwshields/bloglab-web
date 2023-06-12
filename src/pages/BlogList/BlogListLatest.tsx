e37cb2179446afa0bef8ac192056d063b96f166d
cwshields <cwshields2@gmail.com>
2023-06-12 12:27:06 -0500

    Install axios-hooks  - Add AWS backend data

--- src/pages/BlogList/BlogListLatest.tsx
+++ src/pages/BlogList/BlogListLatest.tsx
@@ -1,10 +1,17 @@
 import BlogProps from "../../types/BlogProps";
-import { blogs } from "../../data/blogs";
+// import { blogs } from "../../data/blogs";
+import useAxios from "axios-hooks";
 import BlogCard from "../BlogCard/BlogCard";
 import FadeIn from "react-fade-in";
 
 export default function BlogListLatest() {
-  const blogList = blogs.map((blog: BlogProps, index: number) => (
+  
+  const [{ data, loading, error }/*, refetch*/] = useAxios({
+    method: "GET",
+    url: "https://zea1btt963.execute-api.eu-west-1.amazonaws.com/dev/get-blogs",
+  });
+
+  const blogList = data?.blogs.map((blog: BlogProps, index: number) => (
     <BlogCard
       id={blog.id}
       key={index}
@@ -16,17 +23,25 @@ export default function BlogListLatest() {
       user={blog.user}
     />
   ));
-  console.log(blogList);
+
   return (
     <>
       <FadeIn delay={100}>
-        {blogList.sort((a, b) =>
-          b.props.date
-            .split("/")
-            .join()
-            .localeCompare(a.props.date.split("/").join())
+        {loading ? (
+          <img
+            src="https://www.onwebchat.com/img/spinner.gif"
+            alt="Loading..."
+          />
+        ) : (
+          blogList.sort((a: any, b: any) =>
+            b.props.date
+              .split("/")
+              .join()
+              .localeCompare(a.props.date.split("/").join())
+          )
         )}
       </FadeIn>
+      {error && <div>Error: couldn't load blogs</div>}
     </>
   );
 }
