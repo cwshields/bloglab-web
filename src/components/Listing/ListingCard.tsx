f13818e13877ae758d71e26fccdbca6b41c3723c
cwshields <cwshields2@gmail.com>
2023-06-22 18:29:32 -0500

    Map tag array

--- src/components/Listing/ListingCard.tsx
+++ src/components/Listing/ListingCard.tsx
@@ -3,13 +3,18 @@ import moment from "moment";
 
 export default function ListingCard(props: Listing) {
   const { header, body, user, date, category, tags } = props;
+
+  const tagList = tags.map((tag: string, index: number) => {
+    return <span key={index} className="listing-tag">#{tag}</span>
+  })
+  
   return (
     <div className="listing-card">
       <div className="listing-content">
         <div>
           <h4 className="header">{header}</h4>
           <div className="date">{moment(date).format("MMM DD YYYY")}</div>
-          <div className="tags">#{tags}</div>
+          <div className="tags">{tagList}</div>
         </div>
         <div className="body">{body}</div>
         <div className="user-wrap">
