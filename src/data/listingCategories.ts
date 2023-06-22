60ef9ab313bccb0f4d481baeba14133597ff0242
cwshields <cwshields2@gmail.com>
2023-06-22 17:04:22 -0500

    Create listingCategories data file

---
+++ src/data/listingCategories.ts
@@ -0,0 +1,16 @@
+const listingCategories: Array<string> = [
+  "all",
+  "collabs",
+  "cfp",
+  "forhire",
+  "education",
+  "jobs",
+  "mentors",
+  "mentees",
+  "forsale",
+  "events",
+  "misc",
+  "products",
+];
+
+export default listingCategories;
