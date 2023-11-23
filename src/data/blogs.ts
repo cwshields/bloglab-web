6327977ed8f65c2861a7fd51eddca6ba6b5f5633
cwshields <cwshields2@gmail.com>
2023-11-23 11:16:29 -0600

    Replace js data with axios hook

--- src/data/blogs.ts
+++ src/data/blogs.ts
@@ -1,50 +1,9 @@
-import Blog from "../types/Blog";
+import useGet from "axios-hooks";
 
-const blogs: Array<Blog> = [
-  {
-    id: 1,
-    title:
-      "Where You Could Go In The Light of New Information In The Modern Age",
-    description:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et eos incidunt sunt molestiae officiis fugiat facilis in illo, labore.",
-    readTime: 8,
-    date: "2023-05-23 17:16:17",
-    tags: ["philosophy", "life", "advice"],
-    user: { name: "David Wyllcock", avatar: "https://i.pravatar.cc/50?img=11" },
-  },
-  {
-    id: 2,
-    title:
-      "How To Be An Productive Member of society in an age of Chronic Layoffs",
-    description:
-      "Magnam ipsam totam quaerat enim veniam laborum, assumenda quae ducimus asperiores placeat velit hic at necessitatibus adipisci.",
-    readTime: 7,
-    date: "2023-05-30 09:16:17",
-    tags: ["society", "advice", "productivity"],
-    user: { name: "Carmel Panaman", avatar: "https://i.pravatar.cc/50?img=34" },
-  },
-  {
-    id: 3,
-    title:
-      "My Story From Then To Now From Homelessness To A Fortune 500 Company",
-    description:
-      "Ab pariatur fugit eos rerum blanditiis. Impedit temporibus fuga rem distinctio veritatis magni tempora at provident.",
-    readTime: 16,
-    date: "2023-05-19 14:16:17",
-    tags: ["mystory", "selfhelp", "inspiration"],
-    user: { name: "Ailbert Floyde", avatar: "https://i.pravatar.cc/50?img=33" },
-  },
-  {
-    id: 4,
-    title:
-      "How to bake the most delicious cake you've ever tasted in your life",
-    description:
-      "Ab pariatur fugit eos rerum blanditiis. Impedit temporibus fuga rem distinctio veritatis magni tempora at provident.",
-    readTime: 11,
-    date: "2023-05-28 12:16:17",
-    tags: ["baking", "tasty", "glorious"],
-    user: { name: "Carrol Johnson", avatar: "https://i.pravatar.cc/50?img=41" },
-  },
-];
-
-export default blogs;
+export const useGetBlogData = () => {
+  const [{ data, loading, error }] = useGet(
+    "https://zea1btt963.execute-api.eu-west-1.amazonaws.com/dev/get-blogs"
+  );
+  const [blogsData, blogsloading, blogsError] = [data, loading, error];
+  return { blogsData, blogsloading, blogsError };
+};
