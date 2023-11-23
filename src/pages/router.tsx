a851ee9e59eea710e8c0553e641f0aef9190285f
cwshields <cwshields2@gmail.com>
2023-11-23 11:15:46 -0600

    Add Blog page  - import new blog data

--- src/pages/router.tsx
+++ src/pages/router.tsx
@@ -1,5 +1,6 @@
 // Dependencies
 import { Route, Routes, Navigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 // Page components
 import Home from "./Home/Home";
@@ -20,16 +21,21 @@ import Conduct from "./Conduct/Conduct";
 import Privacy from "./Privacy/Privacy";
 import Terms from "./Terms/Terms";
 import TagList from "./TagList/TagList";
+import Blog from "./Blog/Blog";
 
 // Data
 // TODO: Replace with axios calls
 import tags from "../data/tags";
+import { useGetBlogData } from "../data/blogs";
 import listingCategories from "../data/listingCategories";
 
 // Types
 import TagType from "../types/Tag";
+import BlogType from "../types/Blog";
 
 const Router = () => {
+  
+  const { blogsData, blogsloading, blogsError } = useGetBlogData();
 
   const tagArrayFilter = (arg: string) =>
     tags.filter((tag) => tag.name === arg);
@@ -44,9 +50,34 @@ const Router = () => {
     );
   });
 
-  const listingCategoryMap = listingCategories.map((listing: string, index: number) => {
-    return <Route key={index} path={`${listing}`} element={<ListingCardList />} />
-  })
+  const listingCategoryMap = listingCategories.map(
+    (listing: string, index: number) => {
+      return (
+        <Route key={index} path={`${listing}`} element={<ListingCardList />} />
+      );
+    }
+  );
+
+  const blogRoutes = blogsData?.blogs.map((blog: BlogType, index: number) => {
+    return (
+      <Route
+        key={index}
+        path={`${blog.title}`}
+        element={
+          <Blog
+            id={blog.id}
+            key={index}
+            title={blog.title}
+            description={blog.description}
+            readTime={blog.readTime}
+            tags={blog.tags}
+            date={blog.date}
+            user={blog.user}
+          />
+        }
+      />
+    );
+  });
 
   return (
     <Routes>
@@ -64,6 +95,7 @@ const Router = () => {
         <Route path="all" element={<TagList />} />
         {tagRoutes}
       </Route>
+      <Route path="/blog/*">{blogRoutes}</Route>
       <Route path="/podcasts" element={<Podcasts />} />
       <Route path="/guides" element={<Guides />} />
       <Route path="/faq" element={<FAQ />} />
