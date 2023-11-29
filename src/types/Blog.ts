0847ab5b09f344082f4bb63d27534684983b7cfc
cwshields <cwshields2@gmail.com>
2023-11-28 19:58:39 -0600

    Add properties

--- src/types/Blog.ts
+++ src/types/Blog.ts
@@ -1,11 +1,15 @@
+import { AxiosError } from "axios";
+import Likes from "./Likes";
 import User from "./User";
 
 export default interface Blog {
   id: number;
   title: string;
+  banner?: string;
   description: string;
   readTime: number;
   date: string;
   tags: Array<string>;
   user: User;
+  likes?: Likes;
 }
