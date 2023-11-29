d06abde9cbef0cc61442e2c2553dcb76c6e2301a
cwshields <cwshields2@gmail.com>
2023-11-29 00:21:26 -0600

    Add ListingModal

---
+++ src/components/ListingModal/ListingModal.tsx
@@ -0,0 +1,36 @@
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+import Listing from "../../types/Listing";
+
+interface ListingModalType extends Listing {
+  show: boolean;
+  onHide: () => void;
+}
+
+export default function ListingModal(props: ListingModalType) {
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Modal heading
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <h4>Centered Modal</h4>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+          facilis excepturi magni reiciendis debitis placeat corporis omnis in,
+          exercitationem amet aut quos voluptatem.
+        </p>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
